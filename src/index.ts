3abad910d912a7bd673de878b5b923ce37217db3
Dave Tucker <dave@dtucker.co.uk>
2024-03-13 22:17:46 +0000

    Exec the generate script
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import { Probot } from "probot";
+import { exec } from "node:child_process";
+
+function run(cmd: string) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) return reject(error);
+      if (stderr) return reject(stderr);
+      resolve(stdout);
+    });
+  });
+}
 
 export default (app: Probot) => {
   app.on("pull_request.opened", async (context) => {
@@ -17,23 +28,43 @@ export default (app: Probot) => {
   app.on("issue_comment.created", async (context) => {
     const issueComment = context.payload.comment.body;
     if (issueComment === "@instruct-lab-bot generate") {
+      if (context.payload.issue.pull_request == null) {
+        const issueComment = context.issue({
+          body: `Beep, boop 🤖  Sorry, I can only generate test data for pull requests.`,
+        });
+        await context.octokit.issues.createComment(issueComment);
+        return;
+      }
       const issueComment = context.issue({
-        body: `
-          Beep, boop 🤖  Generating test data for your pull request. This may take a few seconds...
-        `,
+        body:
+          `Beep, boop 🤖  Generating test data for your pull request.\n` +
+          `This may take a few seconds...`,
       });
       await context.octokit.issues.createComment(issueComment);
+
+      try {
+        let prNumber = context.payload.issue.number;
+        await run(`./scripts/generate.sh ${prNumber}`);
+        const issueComment = context.issue({
+          body:
+            `Beep, boop 🤖  The test data has been generated!\n` +
+            `You can now proceed with the review.`,
+        });
+        await context.octokit.issues.createComment(issueComment);
+      } catch (err) {
+        const issueComment = context.issue({
+          body:
+            `Beep, boop 🤖  An error occurred executing your command.\n` +
+            `\`\`\`console\n` +
+            err +
+            `\n\`\`\`\n`,
+        });
+        await context.octokit.issues.createComment(issueComment);
+        return;
+      }
     } else if (issueComment.startsWith("@instruct-lab-bot")) {
       const issueComment = context.issue({
-        body: `
-          Beep, boop 🤖  Sorry, I don't understand that command. Please reply with the following comment:
-
-          @instruct-lab-bot generate
-
-          This will trigger the generation of some test data for your
-          contribution. Once the data is generated, I will let you know
-          and you can proceed with the review.
-        `,
+        body: `Beep, boop 🤖  Sorry, I don't understand that command`,
       });
       await context.octokit.issues.createComment(issueComment);
     }
