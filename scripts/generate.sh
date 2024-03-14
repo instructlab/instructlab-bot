e1089f810b06b16809b66bda1506c868d4f0cdcf
Russell Bryant <rbryant@redhat.com>
2024-03-13 20:02:58 -0400

    generate: Add --work-dir and --venv-path options
    Add --work-dir for the dir to `cd` into, which is where the taxonomy
    repo and lab config should be.
    
    Add --venv-path to optionally source a python venv before running lab.
    
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+VENV_PATH=""
+WORK_DIR=""
+
+usage() {
+    echo "Usage: $0 [--work-dir PATH] [--venv-path PATH] PR_NUMBER"
+    echo
+    echo "  --work-dir PATH: Path to the working directory to move into"
+    echo "  --venv-path PATH: Path to the virtual environment to activate, relative to the working directory"
+    echo "  PR_NUMBER: The number of the pull request to generate data for"
+}
+
 if [ $# -lt 1 ]; then
-    echo "Usage: $0 PR_NUMBER"
-    exit 1
+    usage && exit 1
 fi
-PR_ID="$1"
 
 check_work_dir() {
+    if [ -n "$WORK_DIR" ]; then
+        cd "$WORK_DIR"
+    fi
     if [ ! -d taxonomy ]; then
         echo "taxonomy directory not found"
         exit 1
@@ -15,14 +27,65 @@ check_work_dir() {
         echo "lab config.yaml file not found"
         exit 1
     fi
+    if [ -n "$VENV_PATH" ]; then
+        if [ ! -d "$VENV_PATH" ]; then
+            echo "venv directory not found"
+            exit 1
+        fi
+    fi
+    if [ -n "$WORK_DIR" ]; then
+        if [ ! -d "$WORK_DIR" ]; then
+            echo "work directory not found"
+            exit 1
+        fi
+    fi
 }
 
-check_work_dir
+generate() {
+    check_work_dir
 
-cd taxonomy
-git fetch origin "pull/${PR_ID}/head:pr-${PR_ID}"
-git checkout "pr-${PR_ID}"
-cd ..
-OUTPUT_DIR="generate-pr-${PR_ID}"
-mkdir -p "$OUTPUT_DIR"
-lab generate --output-dir "$OUTPUT_DIR"
+    if [ -n "$VENV_PATH" ]; then
+        source "$VENV_PATH/bin/activate"
+    fi
+    cd taxonomy
+    git fetch origin "pull/${PR_ID}/head:pr-${PR_ID}"
+    git checkout "pr-${PR_ID}"
+    cd ..
+    OUTPUT_DIR="generate-pr-${PR_ID}"
+    mkdir -p "$OUTPUT_DIR"
+    lab generate --output-dir "$OUTPUT_DIR"
+}
+
+# Parse command line arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --help)
+            usage
+            exit 0
+            ;;
+        --venv-path)
+            VENV_PATH="$2"
+            shift
+            ;;
+        --work-dir)
+            WORK_DIR="$2"
+            shift
+            ;;
+        *)
+            if [ -n "$PR_ID" ]; then
+                echo "Invalid argument: $1"
+                exit 1
+            fi
+            PR_ID="$1"
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$PR_ID" ]; then
+    echo "PR_NUMBER is required"
+    usage
+    exit 1
+fi
+
+generate
