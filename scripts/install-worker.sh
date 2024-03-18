4badd9197b7f542b87f75e0614fffa8f00ff31a3
Russell Bryant <rbryant@redhat.com>
2024-03-18 12:18:42 -0400

    install-worker: Install lab + dependencies
    This gets `lab serve` working with NVIDIA GPU support on a Fedora 39
    worker.
    
    Signed-off-by: Russell Bryant <rbryant@redhat.com>

--- scripts/install-worker.sh
+++ scripts/install-worker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 COMMAND=""
+GPU_TYPE=${GPU_TYPE:-""}
 NEXODUS_REG_KEY=${NEXODUS_REG_KEY:-""}
-REDIS_IP=${REDIS_IP:-"127.0.0.1"}
 OS=""
+REDIS_IP=${REDIS_IP:-"127.0.0.1"}
+WORK_DIR=${WORK_DIR:-"${HOME}/instruct-lab-bot"}
 
 supported_envs() {
     echo "Supported Environments:"
@@ -20,8 +22,10 @@ usage() {
     echo
     echo "Options:"
     echo "  -h, --help: Show this help message and exit"
+    echo "  --gpu-type TYPE: Optionally the type of GPU to use. Supported: cuda"
     echo "  --nexodus-reg-key REG_KEY: Optionally a registration key for Nexodus. Ex: https://try.nexodus.io#..."
-    echo "  --redis-ip IP: Optionally the IP address of the Redis server."
+    echo "  --redis-ip IP: Optionally the IP address of the Redis server. Default: ${REDIS_IP}"
+    echo "  --work-dir DIR: Optionally the directory to use for the worker. Default: ${WORK_DIR}"
     echo
     supported_envs
 }
@@ -50,7 +54,31 @@ command_exists() {
 }
 
 install_prereqs_fedora() {
-    sudo dnf install -y git
+    sudo dnf install -y \
+        cmake \
+        gcc \
+        gcc-c++ \
+        git \
+        make \
+        python3 \
+        python3-pip \
+        python3-virtualenv
+
+    if [ "${GPU_TYPE}" = "cuda" ]; then
+        sudo dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/fedora39/x86_64/cuda-fedora39.repo
+        sudo dnf module install -y nvidia-driver:latest-dkms
+        sudo dnf install -y cuda
+        NVIDIA_CHECK=$(sudo lsmod | grep nvidia)
+        if [ -z "${NVIDIA_CHECK}" ]; then
+            echo
+            echo "NVIDIA CUDA installed, but driver not loaded. Please reboot the system to load the NVIDIA driver."
+            echo "Then re-run this script to continue the installation."
+            exit 0
+        fi
+    elif [ -n "${GPU_TYPE}" ]; then
+        echo "Unsupported GPU_TYPE: ${GPU_TYPE}"
+        exit 1
+    fi
 }
 
 install_prereqs() {
@@ -99,10 +127,52 @@ install_nexodus() {
     fi
 }
 
+setup_workdir() {
+    mkdir -p "${WORK_DIR}"
+    cd "${WORK_DIR}" || (echo "Failed to change to work directory: ${WORK_DIR}" && exit 1)
+    if [ ! -d cli ]; then
+        git clone git@github.com:redhat-et/instruct-lab-cli.git cli
+    fi
+    if [ ! -d taxonomy ]; then
+        git clone git@github.com:redhat-et/taxonomy.git
+    fi
+    if [ ! -d venv ]; then
+        python3 -m venv venv
+    fi
+}
+
+install_lab() {
+    cd "${WORK_DIR}" || (echo "Failed to change to work directory: ${WORK_DIR}" && exit 1)
+    # shellcheck disable=SC1091
+    source venv/bin/activate
+    if command_exists "lab"; then
+        echo "lab CLI already installed"
+        return 0
+    fi
+    pip install ./cli
+    if [ "${GPU_TYPE}" = "cuda" ]; then
+        export PATH=/usr/local/cuda-12/bin${PATH:+:${PATH}}
+        export LD_LIBRARY_PATH=/usr/local/cuda-12/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+        CUDACXX="/usr/local/cuda-12/bin/nvcc" \
+            CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCMAKE_CUDA_ARCHITECTURES=native" \
+            FORCE_CMAKE=1 \
+            pip install llama-cpp-python --no-cache-dir --force-reinstall --upgrade
+    elif [ -n "${GPU_TYPE}" ]; then
+        echo "Unsupported GPU_TYPE: ${GPU_TYPE}"
+        exit 1
+    fi
+    if [ ! -f config.yaml ]; then
+        lab init --non-interactive
+    fi
+    lab download
+}
+
 install() {
     check_os
     install_prereqs
     install_nexodus
+    setup_workdir
+    install_lab
 
     echo "Install here"
 }
@@ -114,6 +184,10 @@ fi
 # Parse command line arguments
 while [ $# -gt 0 ]; do
     case "$1" in
+        --gpu-type)
+            GPU_TYPE="$2"
+            shift
+            ;;
         -h|--help)
             usage
             exit 0
@@ -123,6 +197,10 @@ while [ $# -gt 0 ]; do
             shift ;;
         --redis-ip)
             REDIS_IP="$2"
+            shift
+            ;;
+        --work-dir)
+            WORK_DIR="$2"
             shift
             ;;
         *)
